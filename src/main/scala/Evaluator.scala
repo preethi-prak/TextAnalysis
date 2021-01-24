1923074d1b78694fa428b556767534988eaaef8d
Preethi Prakash <Preethi93@icloud.com>
2021-01-24 17:09:01 -0500

    Commit #2 - WikiClearner.scala class

--- src/main/scala/Evaluator.scala
+++ src/main/scala/Evaluator.scala
@@ -1,9 +1,9 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
-import org.apache.hadoop.streaming.StreamXmlRecordReader
-import org.apache.hadoop.mapred.JobConf
+import main.WikiCleaner
 import org.apache.hadoop.io.Text
-import org.apache.hadoop.mapred.FileInputFormat
+import org.apache.hadoop.mapred.{FileInputFormat, JobConf}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.xml.XML
 
 object Evaluator {
   def main(args: Array[String])
@@ -20,6 +20,27 @@ object Evaluator {
     val wikiDocuments = sc.hadoopRDD(jobConf,
       classOf[org.apache.hadoop.streaming.StreamInputFormat],
       classOf[Text], classOf[Text])
+
+    val deHadoopedWikis = wikiDocuments.map(hadoopXML=>hadoopXML._1.toString)
+
+    val rawWikiPages = deHadoopedWikis.map(wikiString=>{
+      val wikiXML = XML.loadString(wikiString)
+      val wikiPageText = (wikiXML \ "revision" \ "text").text
+      WikiCleaner.parse(wikiPageText)
+    })
+
+    val tokenizedWikiData = rawWikiPages.flatMap(wikiText=>wikiText.split(" "))
+    val pertinentWikiData = tokenizedWikiData
+      .map(wikiToken => wikiToken.replaceAll("[.|,|'|\"|?|)|(]", "").trim)
+      .filter(wikiToken=>wikiToken.length > 2)
+
+    val wikiDataSortedByLength = pertinentWikiData.distinct
+      .sortBy(wikiToken=>wikiToken.length, ascending = false)
+      .sample(withReplacement = false, fraction = .01)
+      .keyBy(wikiToken => wikiToken.length)
+
+    wikiDataSortedByLength.collect.foreach(println)
+
 
   }
 
